588afb5ff47fdd7cd595300cbe1e1bb6f7601c91
NetLamp <oplpublishergerman@gmail.com>
2017-01-31 16:13:16 +0100

    Test Update

--- src/test/ifw/Test.java
+++ src/test/ifw/Test.java
@@ -1,12 +1,21 @@
 package test.ifw;
 
+import javafx.stage.Stage;
 import io.github.coalangsoft.ifw.use.InterfaceWorld;
+import io.github.coalangsoft.ifw.wrap.javafx.IfwApplication;
+import io.github.coalangsoft.lib.data.Func;
 
 public class Test {
 	
 	public static void main(String[] args) {
-		String name = InterfaceWorld.findNotAbstract("java.awt.event.ActionListener");
-		System.out.println(name);
+		new IfwApplication(new Func<Stage, Void>() {
+			
+			@Override
+			public Void call(Stage p) {
+				p.show();
+				return null;
+			}
+		}).launch(args);
 	}
 	
 }
